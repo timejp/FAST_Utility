865ba0e81d4c33cfbe5051fa754b26d27c682415
timejp <tokijh@naver.com>
2017-02-06 17:11:35 +0900

    Fragment Tabs 설정

--- app/src/main/java/com/timejh/myutility/MainActivity.java
+++ app/src/main/java/com/timejh/myutility/MainActivity.java
@@ -1,13 +1,77 @@
 package com.timejh.myutility;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    final int TAB_COUNT = 4;
+    OneFragment oneFragment;
+    TwoFragment twoFragment;
+    ThreeFragment threeFragment;
+    FourFragment fourFragment;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Fragment init
+        oneFragment = new OneFragment();
+        twoFragment = new TwoFragment();
+        threeFragment = new ThreeFragment();
+        fourFragment = new FourFragment();
+
+        // TabLayou 정의
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabLayout);
+        tabLayout.addTab(tabLayout.newTab().setText("계산기"));
+        tabLayout.addTab(tabLayout.newTab().setText("단위변환"));
+        tabLayout.addTab(tabLayout.newTab().setText("검색"));
+        tabLayout.addTab(tabLayout.newTab().setText("현재위치"));
+
+        // Fragment Pager 설정
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewPager);
+
+        // Adapter설정
+        PagerAdapter adapter = new PagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
+
+        // 1. Pager가 변경 되었을 때 Tab를 바꿔주는 리스너
+        viewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        // 2. Tab이 변경 되었을 때 페이지를 바꿔주는 리스너
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager));
+    }
+
+    class PagerAdapter extends FragmentStatePagerAdapter {
+
+        public PagerAdapter(FragmentManager fm) {
+            super(fm);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            Fragment fragment = null;
+            switch (position) {
+                case 0: fragment = oneFragment;
+                    break;
+                case 1: fragment = twoFragment;
+                    break;
+                case 2: fragment = threeFragment;
+                    break;
+                case 3: fragment = fourFragment;
+                    break;
+            }
+            return fragment;
+        }
+
+        @Override
+        public int getCount() {
+            return TAB_COUNT;
+        }
     }
 }
